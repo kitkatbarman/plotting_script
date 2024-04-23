a43135a1a6278851274e31ef19fdd8c1935ab9e6
kitkatbarman <161486093+kitkatbarman@users.noreply.github.com>
2024-04-23 00:19:23 -0700

    new updates to games

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from .routes.home import home
 from .routes.investment_app import investment_app
-from .routes.click_button import click_button  # Import the click_button blueprint
+from .routes.click_button import click_button
+from .routes.break_button import break_button  # Import the break_button Blueprint
 
 def create_app():
     app = Flask(__name__)
@@ -9,6 +10,7 @@ def create_app():
 
     app.register_blueprint(home)
     app.register_blueprint(investment_app)
-    app.register_blueprint(click_button)  # Register the click_button blueprint
+    app.register_blueprint(click_button)
+    app.register_blueprint(break_button)  # Register the break_button Blueprint
 
     return app
