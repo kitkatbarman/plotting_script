3e35865ee31dbe58b0157f34bb65a1e936fc8b90
kitkatbarman <joseph.robert.boro@gmail.com>
2024-05-15 02:43:07 -0700

    test
    test

--- app/routes/randomWalk.py
+++ app/routes/randomWalk.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template
+import time
 
 randomWalk = Blueprint('randomWalk', __name__)
 
 @randomWalk.route('/randomWalk')
 def randomWalk_page():
-    return render_template('randomWalk.html')
+    return render_template('randomWalk.html', time=time.time)
